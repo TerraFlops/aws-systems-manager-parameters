698a487c85aff660eb28c283966cfc1c30e966e6
Damian Sloane <damian@capacitorlabs.com>
2020-07-25 15:15:32 +1000

    Work in progress

--- main.tf
+++ main.tf
@@ -9,8 +9,9 @@ resource "aws_ssm_parameter" "parameter_string" {
 
 resource "aws_ssm_parameter" "parameter_secure_string" {
   for_each = tomap({
-    for parameter in var.parameters_string: parameter["name"] => parameter["value"]
+    for parameter in var.parameters_secure_string: parameter["name"] => parameter["value"]
   })
+
   name = each.key
   value = each.value
   type = "SecureString"
